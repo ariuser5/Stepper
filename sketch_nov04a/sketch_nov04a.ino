b6dd772e83cad89d122cd2cb46500dbf53f0527e
Darius_EC <dariusciuverca@gmail.com>
2018-11-04 01:02:46 +0200

    Added Main file.

---
+++ sketch_nov04a/sketch_nov04a.ino
@@ -0,0 +1,30 @@
+
+const int stepPin = 3;
+const int dirPin = 4;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(dirPin, HIGH);
+
+  for(int x = 0; x < 200; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+
+  delay(1000);
+
+  for(int x = 0; x < 200; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+  
+  delay(1000);
+}
